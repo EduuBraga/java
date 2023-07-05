02a66e66ff6ebd4fe0d947599e97f8bf7a4608fe
Eduardo Braga <eduardo.braga467@gmail.com>
2023-07-05 20:44:20 -0300

    Enum

--- Code10/src/empresa/Principal.java
+++ Code10/src/empresa/Principal.java
@@ -1,9 +1,16 @@
 package empresa;
 
+enum Casos {
+	errado, 
+	acertado
+}
+
 public class Principal {
 
 	public static void main(String[] args) {
+		Casos casos;
 		
+		System.out.println(casos);
 	}
 
 }
